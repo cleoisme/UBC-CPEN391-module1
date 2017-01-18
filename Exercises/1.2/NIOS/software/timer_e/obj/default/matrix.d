663cd1448dfd4015998c8e3241deed04da2aabd7
Yueyue Zhang <forest-zhang@hotmail.com>
2017-01-17 20:08:25 -0800

    1.2 completed

---
+++ Exercises/1.2/NIOS/software/timer_e/obj/default/matrix.d
@@ -0,0 +1,35 @@
+obj/default/matrix.o: matrix.c \
+  ../timer_e_bsp//HAL/inc/sys/alt_timestamp.h \
+  ../timer_e_bsp//HAL/inc/alt_types.h \
+  ../timer_e_bsp//drivers/inc/altera_avalon_timer.h \
+  ../timer_e_bsp//HAL/inc/alt_types.h \
+  ../timer_e_bsp//HAL/inc/sys/alt_dev.h ../timer_e_bsp/system.h \
+  ../timer_e_bsp/linker.h ../timer_e_bsp//HAL/inc/sys/alt_llist.h \
+  ../timer_e_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../timer_e_bsp//HAL/inc/sys/alt_llist.h \
+  ../timer_e_bsp//HAL/inc/alt_types.h \
+  ../timer_e_bsp//HAL/inc/sys/alt_warning.h
+
+../timer_e_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../timer_e_bsp//HAL/inc/alt_types.h:
+
+../timer_e_bsp//drivers/inc/altera_avalon_timer.h:
+
+../timer_e_bsp//HAL/inc/alt_types.h:
+
+../timer_e_bsp//HAL/inc/sys/alt_dev.h:
+
+../timer_e_bsp/system.h:
+
+../timer_e_bsp/linker.h:
+
+../timer_e_bsp//HAL/inc/sys/alt_llist.h:
+
+../timer_e_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../timer_e_bsp//HAL/inc/sys/alt_llist.h:
+
+../timer_e_bsp//HAL/inc/alt_types.h:
+
+../timer_e_bsp//HAL/inc/sys/alt_warning.h:
