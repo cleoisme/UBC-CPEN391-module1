def67898558934ddf8316ac397e81c03c85f1a46
Yueyue Zhang <forest-zhang@hotmail.com>
2017-02-10 19:05:58 -0800

    include header file in main

--- Exercises/Main/HikingPal/HikingPal.c
+++ Exercises/Main/HikingPal/HikingPal.c
@@ -1,26 +1,11 @@
-/*
- * "Hello World" example.
- *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
- * device in your system's hardware.
- * The memory footprint of this hosted application is ~69 kbytes by default
- * using the standard reference design.
- *
- * For a reduced footprint version of this template, and an explanation of how
- * to reduce the memory footprint for a given application, see the
- * "small_hello_world" template.
- *
- */
-
 #include <stdio.h>
 #include "../../1.7/NIOS_II_System_391_For_Students/software/graphics2/graphics.h"
+#include "../../1.8b/NIOS_II_System_391_For_Students/software/cpen_391_exercise_1_8/SDCard_Test_Program.h"
 
 int main()
 {
   printf("Hello from Nios II!\n");
-  TestShapes();
-
+  //TestShapes();
+  TestSDCard();
   return 0;
 }
